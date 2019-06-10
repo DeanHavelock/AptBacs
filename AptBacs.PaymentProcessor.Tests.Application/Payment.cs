2280367f8c797a3f0c6b7c46cf2f9bed69a40b51
Dean H <deanhavelock@hotmail.com>
2019-06-10 20:44:46 +0100

    red to green test method name updated, red test inserted

---
+++ AptBacs.PaymentProcessor.Tests.Application/Payment.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AptBacs.PaymentProcessor.Tests.Application
+{
+    public class Payment
+    {
+        [Fact]
+        public void Given_ValidCsvFile_When_SubmittedToPaymentApiForProcessing_Then_PaymentRecorded()
+        {
+            Assert.True(false);
+        }
+    }
+}
